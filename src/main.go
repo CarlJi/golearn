2b4b22e661e45383ab52a1a9dfaddc577ca53099
jichangjun <jichangjun@qiniu.com>
2016-03-09 23:35:00 +0800

    update

--- src/main.go
+++ src/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
 	s := "ABCDEFGABCDEFGF"
-	res := FieldsFunc(s, isSplict)
+	res := strings.FieldsFunc(s, isSplict)
 
 	fmt.Printf("Result: %v ", res)
 }
@@ -16,44 +19,4 @@ func isSplict(char rune) bool {
 		return true
 	}
 	return false
-}
-
-// FieldsFunc splits the string s at each run of Unicode code points c satisfying f(c)
-// and returns an array of slices of s. If all code points in s satisfy f(c) or the
-// string is empty, an empty slice is returned.
-// FieldsFunc makes no guarantees about the order in which it calls f(c).
-// If f does not return consistent results for a given c, FieldsFunc may crash.
-func FieldsFunc(s string, f func(rune) bool) []string {
-	// First count the fields.
-	n := 0
-	inField := false
-	for _, rune := range s {
-		wasInField := inField
-		inField = !f(rune)
-		if inField && !wasInField {
-			n++
-		}
-	}
-
-	fmt.Println("n equals", n)
-
-	// Now create them.
-	a := make([]string, n)
-	na := 0
-	fieldStart := -1 // Set to -1 when looking for start of field.
-	for i, rune := range s {
-		if f(rune) {
-			if fieldStart >= 0 {
-				a[na] = s[fieldStart:i]
-				na++
-				fieldStart = -1
-			}
-		} else if fieldStart == -1 {
-			fieldStart = i
-		}
-	}
-	if fieldStart >= 0 { // Last field might end at EOF.
-		a[na] = s[fieldStart:]
-	}
-	return a
 }
