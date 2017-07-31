ab3690340351daed575af8c9f05f04f2ec217f3d
jichangjun <jinsdu@outlook.com>
2017-07-31 23:01:20 +0800

    add simple factory pattern

--- src/carlji.com/books/go-design-patterns/factory/factory.go
+++ src/carlji.com/books/go-design-patterns/factory/factory.go
@@ -1,1 +1,41 @@
 package factory
+
+import (
+	"errors"
+	"fmt"
+)
+
+type PaymentMethod interface {
+	Pay(count float32) string
+}
+
+const (
+	Card  = 10.0
+	Debit = 30.0
+)
+
+type CardMethod struct {
+}
+
+func (c *CardMethod) Pay(count float32) string {
+	return fmt.Sprintf("using %v", CardMethod{})
+}
+
+type DebitMethod struct {
+}
+
+func (d *DebitMethod) Pay(count float32) string {
+	return fmt.Sprintf("using %v", DebitMethod{})
+}
+
+func GetPaymentMethod(count int) (PaymentMethod, error) {
+	switch count {
+	case Card:
+		return new(CardMethod), nil
+	case Debit:
+		return new(DebitMethod), nil
+
+	default:
+		return nil, errors.New("unkown payment type")
+	}
+}
