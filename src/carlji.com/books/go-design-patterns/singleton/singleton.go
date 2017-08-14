bd257d38760a932269b8b2be593a7c23006e029f
jichangjun <jinsdu@outlook.com>
2017-08-14 14:48:58 +0800

    another singleton design pattern implementation

--- src/carlji.com/books/go-design-patterns/singleton/singleton.go
+++ src/carlji.com/books/go-design-patterns/singleton/singleton.go
@@ -17,3 +17,10 @@ func (s *singleton) AddOne() int {
 	s.count += 1
 	return s.count
 }
+
+// Another singleton implementation
+var Instance2 *singleton
+
+func init() {
+	Instance2 = new(singleton)
+}
