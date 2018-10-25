19e7d2e7eabb0ddd89b09e8fa335b4fa293942c3
jichangjun <jinsdu@outlook.com>
2018-10-25 09:47:56 +0800

    Revert "add files to test prow"
    This reverts commit 45c9471e7d3b844a191480c65f84c8e76a6e9228.

--- prow/deploy-prow.sh
+++
@@ -1,6 +0,0 @@
-set -x
-
-kubectl create secret generic hmac-token --from-file=hmac=hmac.txt
-kubectl create secret generic oauth-token --from-file=oauth=oauth.txt
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/test-infra/master/prow/cluster/starter.yaml
