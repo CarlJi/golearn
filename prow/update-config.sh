45c9471e7d3b844a191480c65f84c8e76a6e9228
jichangjun <jinsdu@outlook.com>
2018-10-25 09:46:56 +0800

    add files to test prow

---
+++ prow/update-config.sh
@@ -0,0 +1,13 @@
+set -x 
+
+kubectl create configmap plugins \
+  --from-file=plugins.yaml=plugins.yaml --dry-run -o yaml \
+  | kubectl replace configmap plugins -f -
+
+
+kubectl create configmap config \
+  --from-file=config.yaml=config.yaml --dry-run -o yaml | kubectl replace configmap config -f -
+
+kubectl scale deploy hook --replicas=0
+sleep 1
+kubectl scale deploy hook --replicas=1
